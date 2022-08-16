fa4518a85686c79e02fec7c6489b7e9f526bb341
anreh <anurehka@gmail.com>
2022-08-16 12:45:33 +0800

    8/16/2022 1245 - data binding

---
+++ app/src/main/java/com/example/myhelloworld/GameInfo.kt
@@ -0,0 +1,4 @@
+package com.example.myhelloworld
+
+data class GameInfo(var playerName: String="", var totalScore: String="0")
+
--- app/src/main/java/com/example/myhelloworld/MainActivity.kt
+++ app/src/main/java/com/example/myhelloworld/MainActivity.kt
@@ -6,25 +6,37 @@ import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.*
+import androidx.databinding.DataBindingUtil
+import com.example.myhelloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-   lateinit var diceImg : ImageView //cannot findViewById here, because haven't set the layout
+   /*lateinit var diceImg : ImageView //cannot findViewById here, because haven't set the layout
    lateinit var numberText : TextView
    lateinit var editPlayerName : EditText
-   lateinit var playerNameTV : TextView
+   lateinit var playerNameTV : TextView*/
 //    var diceImg : ImageView? = null
+    lateinit var binding: ActivityMainBinding
+    private val gameInfo: GameInfo = GameInfo("Ong", "0")
+    private var totalScore: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        diceImg = findViewById(R.id.diceImage)
-        numberText = findViewById(R.id.numberText)
-        editPlayerName = findViewById(R.id.editPlayerNameET)
-        playerNameTV = findViewById(R.id.playerNameTV)
-        val rollButton: Button = findViewById(R.id.rollButton)
-        rollButton.setOnClickListener { rollDice() }
-        val updateButton : Button = findViewById(R.id.updatePlayerNameBtn)
-        updateButton.setOnClickListener { updatePlayerName(it) }
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+//        diceImg = findViewById(R.id.diceImage)
+//        numberText = findViewById(R.id.numberText)
+//        editPlayerName = findViewById(R.id.editPlayerNameET)
+//        playerNameTV = findViewById(R.id.playerNameTV)
+//        val rollButton: Button = findViewById(R.id.rollButton)
+//        rollButton.setOnClickListener { rollDice() }
+
+        binding.gameInfo = gameInfo //in front gameInfo is from activity_main.xml data, back is from this page
+        binding.rollButton.setOnClickListener { rollDice() }
+//        val updateButton : Button = findViewById(R.id.updatePlayerNameBtn)
+//        updateButton.setOnClickListener { updatePlayerName(it) }
+        binding.updatePlayerNameBtn.setOnClickListener { updatePlayerName(it) }
     }
 
         private fun rollDice(){
@@ -34,7 +46,8 @@ class MainActivity : AppCompatActivity() {
             val randomNum = (1..6).random()
 
 //            val numberText: TextView = findViewById(R.id.numberText)
-            numberText.text = randomNum.toString()
+            totalScore += randomNum
+           binding.numberText.text = totalScore.toString()
 //            val diceImg : ImageView = findViewById(R.id.diceImage)
             val imgSrc = when (randomNum){
                 1 -> R.drawable.dice_1
@@ -44,7 +57,7 @@ class MainActivity : AppCompatActivity() {
                 5 -> R.drawable.dice_5
                 else -> R.drawable.dice_6
             }
-            diceImg.setImageResource(imgSrc)
+           binding.diceImage.setImageResource(imgSrc)
 
 
             Toast.makeText(this, randomNum.toString(),
@@ -52,13 +65,17 @@ class MainActivity : AppCompatActivity() {
         }
 
     private fun updatePlayerName(view: View){
-        playerNameTV.text = editPlayerName.text
+        binding.apply {
+            gameInfo?.playerName = editPlayerNameET.text.toString()
+            invalidateAll()
+        }
+//        playerNameTV.text = editPlayerName.text
+//        editPlayerName.text.clear()
+//        editPlayerName.clearFocus()
 
-        editPlayerName.text.clear()
-        editPlayerName.clearFocus()
+//        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//        imm.hideSoftInputFromWindow(view.windowToken, 0)
 
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
 }
